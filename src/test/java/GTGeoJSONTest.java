26c100146f4e72b3022ec7b75659285feebda9c8
arnevogt <arne.vogt@hs-bochum.de>
2018-07-25 12:06:52 +0200

    Change Attribution

--- src/test/java/GTGeoJSONTest.java
+++ src/test/java/GTGeoJSONTest.java
@@ -17,10 +17,8 @@ import com.vividsolutions.jts.geom.Polygon;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-/**
  *
- * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ * @author <a href="mailto:arne.vogt@hs-bochum.de">Arne Vogt</a>
  */
 public class GTGeoJSONTest {
 
